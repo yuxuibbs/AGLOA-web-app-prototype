c8df2ab3e63ea88453830ed7740828461025eb2e
yuxuanc <yuxuanc@umich.edu>
2019-08-21 19:57:27 -0400

    add results + player info pages, insert images, use js to load navbar and footer

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    document.getElementById('insertFooter').innerHTML =`
+    <footer>
+        <hr>     
+        <img src="img/AGLOA_logo_footer_44x30.png" alt="Academic Games Leagues of America footer" /></a> Copyright © 2019 <a href="https://www.agloa.org/" title="Academic Games Leagues of America" ><span>Academic Games Leagues of America</span></a>. All Rights Reserved.
+    </footer>
+        `;
+});
