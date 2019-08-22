81308daf4f61f962ed4a2c984c5363f76675cb46
yuxuanc <yuxuanc@umich.edu>
2019-08-21 21:20:02 -0400

    make mobile look better and change container to container-fluid

--- js/navbar.js
+++ js/navbar.js
@@ -1,62 +1,50 @@
 $(document).ready(function() {
     document.getElementById('insertNavBar').innerHTML =`
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-      <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.html">
-            <div class="pull-left">
-                <img src="img/cropped-AGLOA_logo_header_133x90.png" alt="AGLOA logo">
-            </div>
-            <div id="title" class="pull-right">
-                <h1 class="site-title">
-                    <a href="index.html" rel="home">Academic Games Leagues of America</a>
-                </h1>
-                <div class="site-description">
-                    <ul>
-                        <li>
-                            AGLOA | Promoting Excellence Through Academic Competition
-                        </li>
-                        <li class="facebook">
-                            <a href="http://facebook.com/AGLOA" title="Academic Games Leagues of America on Facebook" target="_blank"><i class="fab fa-facebook-square"></i></a>
-                        </li>
-                        <li class="twitter">
-                            <a href="http://twitter.com/AGLOA" title="Academic Games Leagues of America on Twitter" target="_blank"><i class="fab fa-twitter-square"></i></a>
-                        </li>
-                        <li class="youtube">
-                            <a href="http://youtube.com/AGLOAczeller" title="Academic Games Leagues of America on YouTube" target="_blank"><i class="fab fa-youtube-square"></i></a>
-                        </li>
-                        <li class="instagram">
-                            <a href="http://instagram.com/AGLOA" title="Academic Games Leagues of America on Instagram" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        <li class="email">
-                            <a href="mailto:info@agloa.org" title="Academic Games Leagues of America on Email" target="_blank"><i class="fa fa-envelope"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+            <img id="logo" src="img/cropped-AGLOA_logo_header_133x90.png" alt="AGLOA logo">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="schedule.html">Schedule</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="map.html">Map</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="player_info.html">Player Information</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="results.html">Results</a>
-            </li>
-          </ul>
+        <div>
+            <h1 class="site-title">
+                <a href="index.html" rel="home">Academic Games Leagues of America</a>
+            </h1>
+            <div class="site-description">
+                Promoting Excellence Through Academic Competition
+                <br/>
+                <a href="http://facebook.com/AGLOA" title="Academic Games Leagues of America on Facebook" target="_blank"><i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="http://twitter.com/AGLOA" title="Academic Games Leagues of America on Twitter" target="_blank"><i class="fab fa-twitter-square"></i>
+                </a>
+                <a href="http://youtube.com/AGLOAczeller" title="Academic Games Leagues of America on YouTube" target="_blank"><i class="fab fa-youtube-square"></i>
+                </a>
+                <a href="http://instagram.com/AGLOA" title="Academic Games Leagues of America on Instagram" target="_blank"><i class="fab fa-instagram"></i>
+                </a>
+                <a href="mailto:info@agloa.org" title="Academic Games Leagues of America on Email" target="_blank"><i class="fa fa-envelope"></i>
+                </a>
+            </div>
         </div>
-      </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="schedule.html">Schedule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="map.html">Map</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="player_info.html">Player Information</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="results.html">Results</a>
+                </li>
+            </ul>
+        </div>
     </nav>
     `;
 });
